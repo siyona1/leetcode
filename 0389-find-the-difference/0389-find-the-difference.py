548f238e7a224d4678e7a84805e2adfa4158f213
Siyona <109352535+siyona1@users.noreply.github.com>
2024-05-15 01:51:49 +0530

    Time: 11 ms (92.67%), Space: 11.7 MB (43.63%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        c = 0
+        for cs in s: c ^= ord(cs) #ord is ASCII value
+        for ct in t: c ^= ord(ct)
+        return chr(c) #chr = convert ASCII into character
+        
